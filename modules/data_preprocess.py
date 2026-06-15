8f4b74bbc3f6e6f58f9e00ee132e88d47b36b120
VishSeran <vishwa.sovis@gmail.com>
2026-06-15 17:11:48 +0530

    project data preprocess updated

--- modules/data_preprocess.py
+++ modules/data_preprocess.py
@@ -3,12 +3,27 @@ from llama_index.core import Document
 from llama_index.core.node_parser import SentenceSplitter
 import json
 from modules.config import CHUNK_SIZE, CHUNK_OVERLAP
+from logging import Logger
 
-def split_json_data(profile_json:dict[str,Any]):
+logger = Logger(name="logger")
+
+def split_json_data(profile_json:dict[str,Any])-> list:
     
-    profile_json_data = json.dumps(profile_json)
-    documents = Document(text=profile_json_data)
+    try:
+        
+        profile_json_data = json.dumps(profile_json)
+        documents = Document(text=profile_json_data)
+        
+        splitter = SentenceSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+        nodes = splitter.get_nodes_from_documents([documents],show_progress=True)
+        
+        logger.info(f"Created {len(nodes)} of nodes from profile_json data")
+        return nodes
     
-    splitter = SentenceSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
-    nodes = splitter.get_nodes_from_documents(documents=documents,
-                                              show_progress=True)
+    except Exception as e:
+        logger.error(f"error during creating nodes: {e}")
+        print(f"Unknown error: {e}")
+        return []
+        
+    
+    
