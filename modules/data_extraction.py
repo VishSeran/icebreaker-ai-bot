a9cd9ffc510b28ee1a05dbb990823c6062183d85
VishSeran <vishwa.sovis@gmail.com>
2026-06-15 15:48:23 +0530

    project data extraction updated

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -29,8 +29,20 @@ def data_extraction(linkedIn_url:str,
                 "Authorization": f"{api_key}"
             }
             
+            params = {
+                "url": linkedIn_url,
+                "fallback_to_cache": "on-error",
+                "use_cache": "if-present",
+                "skills": "include",
+                "inferred_salary": "include",
+                "personal_email": "include",
+                "personal_contact_number": "include"
+            }
             
-            response = requests.get(url=)
+            logger.info(f"Sending API request to fetch the profile details")
+            
+            response = requests.get(api_endpoint, headers=headers, params=params)
+            
         
         return response
         
