a047cf30c961e1e1df113e9dbd946743114bedcd
VishSeran <vishwa.sovis@gmail.com>
2026-06-15 15:45:47 +0530

    project data extraction updated

---
+++ modules/data_extraction.py
@@ -0,0 +1,43 @@
+
+from typing import Any, Optional
+import requests
+from modules.config import MOCK_DATA_URL
+import time
+from logging import Logger
+
+logger = Logger(name="logger")
+
+
+def data_extraction(linkedIn_url:str,
+                    api_key:Optional[str]=None,
+                    ismock:bool = False) -> dict[str,Any]:
+    
+    start_time = time.time()
+    
+    try: 
+        if ismock:
+            logger.info("Using mock data from a premade JSON file")
+            response = requests.get(url=MOCK_DATA_URL, timeout=30)
+            
+        else:
+            if not api_key:
+                raise ValueError("api key is empty or none")
+            
+            logger.info("Starting to extract the LinkedIn profile...")
+            api_endpoint = "https://api.linkedin.com/v2/me"
+            headers = {
+                "Authorization": f"{api_key}"
+            }
+            
+            
+            response = requests.get(url=)
+        
+        return response
+        
+    except ValueError as e:
+        print(f"Value error: {e}")
+        return {}
+    
+    except Exception as e:
+        print(f"Unknown error: {e}")
+        return {}
